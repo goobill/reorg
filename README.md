04537de0e009c62b72415035386090898aaa1782
bill <will.fussey@gmail.com>
2024-12-14 18:48:21 +0000

    remove mongo import

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 
 https://learn.microsoft.com/en-gb/azure/azure-functions/functions-reference-node?tabs=javascript%2Cwindows%2Cazure-cli&pivots=nodejs-model-v4
+
+
+Node 22 with Linux OS did not work
+
+Use Node 20 with Windows
--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const { app } = require('@azure/functions');
-const mongo = require("mongodb");
+// const mongo = require("mongodb");
 
 const uri = process.env["MONGODB_ATLAS_URI"];
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new mongo.MongoClient(uri);
+// const client = new mongo.MongoClient(uri);
 
 app.http('metrics', {
     // route: 'metrics',
@@ -16,22 +16,23 @@ app.http('metrics', {
     handler: async (request, context) => {
         try {
             // Connect the client to the server	(optional starting in v4.7)
-            await client.connect();
+            // await client.connect();
             
-            const db = client.db("reorg");
-            const collection = db.collection("metrics");
+            // const db = client.db("reorg");
+            // const collection = db.collection("metrics");
 
-            // Calculate the datetime for one week ago
-            const oneWeekAgo = new Date();
-            oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+            // // Calculate the datetime for one week ago
+            // const oneWeekAgo = new Date();
+            // oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
-            // Query to fetch records from the last week, sorted by datetime in descending order
-            const query = { datetime: { $gte: oneWeekAgo } };
-            const options = {
-                sort: { datetime: -1 }, // Sort descending
-            };
+            // // Query to fetch records from the last week, sorted by datetime in descending order
+            // const query = { datetime: { $gte: oneWeekAgo } };
+            // const options = {
+            //     sort: { datetime: -1 }, // Sort descending
+            // };
 
-            const results = await collection.find(query, options).toArray();
+            // const results = await collection.find(query, options).toArray();
+            const results = []
 
             return { status: 200, jsonBody: results };
         } catch (e) {
@@ -39,8 +40,8 @@ app.http('metrics', {
             return { jsonBody: [] };
         } finally {
             // Ensures that the client will close when you finish/error
-            await client.close();
-            context.done()
+            // await client.close();
+            // context.done()
         }
     }
 });
