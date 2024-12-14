515f3335b74b0b98eb9e43cc334dae0da3f49896
bill <will.fussey@gmail.com>
2024-12-14 15:59:08 +0000

    first attempt

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # Reorg Private
+
+
+https://learn.microsoft.com/en-gb/azure/azure-functions/functions-reference-node?tabs=javascript%2Cwindows%2Cazure-cli&pivots=nodejs-model-v4
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const { app } = require('@azure/functions');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env["MONGODB_ATLAS_URI"];
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+app.http('metrics', {
+    route: 'metrics',
+    methods: ['GET'],
+    authLevel: 'anonymous',
+    return: output.generic({
+        type: 'http'
+    }),
+    handler: async (request, context) => {
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+            
+            const db = client.db("reorg");
+            const collection = db.collection("metrics");
+
+            // Calculate the datetime for one week ago
+            const oneWeekAgo = new Date();
+            oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
+
+            // Query to fetch records from the last week, sorted by datetime in descending order
+            const query = { datetime: { $gte: oneWeekAgo } };
+            const options = {
+                sort: { datetime: -1 }, // Sort descending
+            };
+
+            const results = await collection.find(query, options).toArray();
+
+            return results;
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    }
+});
