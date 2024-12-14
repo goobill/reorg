6e64ca72b886770c72ece7f5e67392bcf3c0d200
bill <will.fussey@gmail.com>
2024-12-14 18:41:13 +0000

    convert to require imports

--- src/index.js
+++ src/index.js
@@ -1,66 +1,46 @@
-// import { app } from '@azure/functions';
-// import { MongoClient, ServerApiVersion } from 'mongodb';
-
-// const uri = process.env["MONGODB_ATLAS_URI"];
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// app.http('metrics', {
-//     // route: 'metrics',
-//     methods: ['GET'],
-//     authLevel: 'anonymous',
-//     return: output.generic({
-//         type: 'http'
-//     }),
-//     handler: async (request, context) => {
-//         try {
-//             // Connect the client to the server	(optional starting in v4.7)
-//             await client.connect();
-            
-//             const db = client.db("reorg");
-//             const collection = db.collection("metrics");
-
-//             // Calculate the datetime for one week ago
-//             const oneWeekAgo = new Date();
-//             oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-//             // Query to fetch records from the last week, sorted by datetime in descending order
-//             const query = { datetime: { $gte: oneWeekAgo } };
-//             const options = {
-//                 sort: { datetime: -1 }, // Sort descending
-//             };
-
-//             const results = await collection.find(query, options).toArray();
-
-//             return { jsonBody: results };
-//         } catch (e) {
-//             context.log.error(`Error: ${e.message}`)
-//             return { jsonBody: [] };
-//         } finally {
-//             // Ensures that the client will close when you finish/error
-//             await client.close();
-//             context.done()
-//         }
-//     }
-// });
-
 const { app } = require('@azure/functions');
+const mongo = require("mongodb");
 
-app.http('httpTrigger1', {
-    methods: ['GET', 'POST'],
+const uri = process.env["MONGODB_ATLAS_URI"];
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new mongo.MongoClient(uri);
+
+app.http('metrics', {
+    // route: 'metrics',
+    methods: ['GET'],
     authLevel: 'anonymous',
+    return: output.generic({
+        type: 'http'
+    }),
     handler: async (request, context) => {
-        context.log(`Http function processed request for url "${request.url}"`);
+        try {
+            // Connect the client to the server	(optional starting in v4.7)
+            await client.connect();
+            
+            const db = client.db("reorg");
+            const collection = db.collection("metrics");
 
-        const name = request.query.get('name') || (await request.text()) || 'world';
+            // Calculate the datetime for one week ago
+            const oneWeekAgo = new Date();
+            oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
 
-        return { body: `Hello, ${name}!` };
-    },
+            // Query to fetch records from the last week, sorted by datetime in descending order
+            const query = { datetime: { $gte: oneWeekAgo } };
+            const options = {
+                sort: { datetime: -1 }, // Sort descending
+            };
+
+            const results = await collection.find(query, options).toArray();
+
+            return { status: 200, jsonBody: results };
+        } catch (e) {
+            context.log.error(`Error: ${e.message}`)
+            return { jsonBody: [] };
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+            context.done()
+        }
+    }
 });
