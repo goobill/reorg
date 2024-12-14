bbd7fb53af1d7efd4c1929ec70602a75ac71bd6d
bill <will.fussey@gmail.com>
2024-12-14 19:03:05 +0000

    move client into function

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 const { app } = require('@azure/functions');
-const mongo = require("mongodb");
-
-const uri = process.env["MONGODB_ATLAS_URI"];
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new mongo.MongoClient(uri);
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.http('metrics', {
-    route: 'metrics',
+    // route: 'metrics',
     methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
+        const uri = process.env["MONGODB_ATLAS_URI"];
+
+        // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+        const client = new MongoClient(uri, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+
         try {
             // Connect the client to the server	(optional starting in v4.7)
             await client.connect();
